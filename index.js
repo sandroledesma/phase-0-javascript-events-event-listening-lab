fb34fcc6a54e6e4fbd00aadc4715c6abf03fa035
Alessandro <sandroledesma@gmail.com>
2024-03-02 11:14:32 -0500

    add files

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
-}
+    const someElement = document.getElementById('button');
+    someElement.addEventListener('click', function() {
+      console.log('Element clicked!');
+    });
+  }
+  
+  addingEventListener();
